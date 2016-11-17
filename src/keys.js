6990d10a22c4c180c463a2f1969256c3470fe622
raphamorim <rapha850@gmail.com>
2016-11-17 04:37:55 -0200

    drag and drop files / astrofish theme

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,26 @@
 function Editor() {
+  var content = document.querySelector('.mirror-code')
+
   this.update = function(){
     var cursor = document.querySelector('.cursor');
     cursor.classList.remove('animate');
 
-    var mirror = document.querySelector('.mirror-code')
-    mirror.focus()
-    console.log(mirror.value)
+    content.focus()
 
     var text = document.querySelector('.code');
-    text.innerHTML = Prism.highlight(mirror.value, Prism.languages.javascript);
-    setTimeout(function(){
-      cursor.classList.add('animate');
-    }, 400);
+    text.innerHTML = Prism.highlight(content.value, Prism.languages.javascript);
+
+    setTimeout(function() {
+      window.scrollTo(0, text.offsetHeight);
+      setTimeout(function(){
+        cursor.classList.add('animate');
+      }, 400);
+    }, 20);
+  }
+
+  this.newTab = function(filename, data) {
+    content.value = data;
+    this.update();
   }
 }
 var editor = new Editor();
@@ -19,7 +28,7 @@ var editor = new Editor();
 // key("a", function() {console.log(1)})
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    var mirror = document.querySelector('.mirror-code')
+    var mirror = document.querySelector('.mirror-code');
 
     mirror.value = "var a = 'Hi, this retro.'; \n" +
     "document.body.addEventListener('keydown', function(event) {\n" +
@@ -28,8 +37,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
     "});";
     mirror.focus()
 
-    document.body.addEventListener('keyup', () => { mirror.focus })
-    document.body.addEventListener('keydown', () => { mirror.focus })
+
+    function updateScreen() {
+      mirror.focus();
+    }
+
+    document.body.addEventListener('keyup', () => { updateScreen() })
+    document.body.addEventListener('keydown', () => { updateScreen() })
     editor.update()
     mirror.addEventListener('input', editor.update.bind(this))
 });
+
+
+document.ondragover = document.ondrop = (ev) => {
+  ev.preventDefault()
+}
+
+document.body.ondrop = (ev) => {
+  var filepath = ev.dataTransfer.files[0].path;
+  var fs = require('fs');
+  console.log(filepath)
+  fs.readFile(filepath, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    editor.newTab(filepath, data);
+  });
+  ev.preventDefault()
+}
