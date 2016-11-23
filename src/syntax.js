2339b1ff637b0cd948dbff915ceeac5d0c29eb41
Raphael Amorim <rapha850@gmail.com>
2016-11-22 22:03:29 -0200

    add syntax and rewrite main.js

---
+++ src/syntax.js
@@ -0,0 +1,94 @@
+var syntaxes = {
+  "js": {
+    mode: "text/javascript"
+  },
+  "json": {
+    mode: "text/javascript"
+  },
+  "md": {
+    mode: 'markdown',
+  },
+  "markdown": {
+    mode: 'markdown',
+  },
+  "ts": {
+    mode: "text/typescript"
+  },
+  "py": {
+    mode: {
+      name: "python",
+      version: 3,
+      singleLineStringErrors: false
+    },
+    indentUnit: 4
+  },
+  "pyx": {
+    mode: {
+      name: "text/x-cython",
+      version: 2,
+      singleLineStringErrors: false
+    },
+    indentUnit: 4
+  },
+  "rb": {
+    mode: "text/x-ruby",
+    indentUnit: 4
+  },
+  "rs": {
+    lineWrapping: true,
+    indentUnit: 4,
+    mode: "rust"
+  },
+  "rpm": {
+    mode: {
+      name: "rpm-spec"
+    },
+    indentUnit: 4
+  },
+  "sh": {
+    mode: 'shell'
+  },
+  "c": {
+    mode: "text/x-csrc"
+  },
+  "cpp": {
+    mode: "text/x-c++src"
+  },
+  "cpp": {
+    mode: "text/x-c++src"
+  },
+  "java": {
+    mode: "text/x-java"
+  },
+  "m": {
+    mode: "text/x-objectivec"
+  },
+  "h": {
+    mode: "text/x-objectivec"
+  },
+  "scala": {
+    mode: "text/x-scala"
+  },
+  "sc": {
+    mode: "text/x-scala"
+  },
+  "kt": {
+    mode: "text/x-kotlin"
+  },
+  "kts": {
+    mode: "text/x-kotlin"
+  },
+  "ceylon": {
+    mode: "text/x-ceylon"
+  },
+  "bnn": {
+    name: "banana",
+    mode: "text/scss"
+  },
+  "go": {
+    indentUnit: 8,
+    tabSize: 8,
+    indentWithTabs: true,
+    mode: "text/x-go"
+  }
+}
