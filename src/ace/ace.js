c231cd7bb5d534f756bb115c0dec774d494b6419
Raphael Amorim <rapha850@gmail.com>
2016-12-01 20:53:51 -0200

    fix bugs to build

--- src/ace/ace.js
+++ src/ace/ace.js
@@ -1,41 +1,4 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Distributed under the BSD license:
- *
- * Copyright (c) 2010, Ajax.org B.V.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Ajax.org B.V. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL AJAX.ORG B.V. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ***** END LICENSE BLOCK ***** */
-
-/**
- * Define a module along with a payload
- * @param module a name for the payload
- * @param payload a function to call with (require, exports, module) params
- */
-
 var fs = require('fs');
-
 (function() {
 
     var ACE_NAMESPACE = "ace";
@@ -15631,7 +15594,7 @@ ace.define("ace/virtual_renderer", ["require", "exports", "module", "ace/lib/oop
     var RenderLoop = require("./renderloop").RenderLoop;
     var FontMetrics = require("./layer/font_metrics").FontMetrics;
     var EventEmitter = require("./lib/event_emitter").EventEmitter;
-    var editorCss = fs.readFileSync("assets/stylesheet/editor.css");
+    var editorCss = fs.readFileSync(__dirname + "/assets/stylesheet/editor.css", 'utf8')
 
     dom.importCssString(editorCss, "ace_editor.css");
 
--- src/ace/theme-monokai.js
+++ src/ace/theme-monokai.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 ace.define("ace/theme/monokai",["require","exports","module","ace/lib/dom"], function(require, exports, module) {
 exports.isDark = true;
 exports.cssClass = "ace-monokai";
-exports.cssText = fs.readFileSync('src/themes/monokai.css');
+exports.cssText = fs.readFileSync(__dirname + '/src/themes/monokai.css');
 var dom = require("../lib/dom");
 dom.importCssString(exports.cssText, exports.cssClass);
 });
