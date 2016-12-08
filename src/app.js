ec1f59a895414e2997dd0e5c8f5f9bee998dda0c
Raphael Amorim <rapha850@gmail.com>
2016-12-08 17:04:28 -0200

    finish modal file

--- src/app.js
+++ src/app.js
@@ -35,14 +35,58 @@ let cachedFiles = [];
 
 modalSearch.addEventListener('keydown', function(e) {
   // enter
-  // if (e.keyCode === 13) {
-  //   // openFile
-  // }
+  if (e.keyCode === 13) {
+    enterModalItems()
+  }
+
+  // up key
+  if (e.keyCode === 38) {
+    updateModalItems('up')
+  }
+
+  // down key
+  if (e.keyCode === 40) {
+    updateModalItems('down')
+  }
 
   // esc
   if (e.keyCode === 27)
     modal.classList.remove('visible');
-})
+});
+
+function enterModalItems() {
+  modal.classList.remove('visible');
+  const item = modalItems.querySelector('.active');
+  retro.openFile(item.getAttribute('data-path'));
+}
+
+function indexInParent(node) {
+  var children = node.parentNode.childNodes;
+  var num = 0;
+  for (var i = 0; i < children.length; i++) {
+    if (children[i] == node) return num;
+    if (children[i].nodeType == 1) num++;
+  }
+  return -1;
+}
+
+function updateModalItems(key) {
+  const item = modalItems.querySelector('.active');
+  let currentPos = indexInParent(item);
+  
+  let nextPos = (key === 'down') ? ++currentPos : --currentPos;
+  if (currentPos < 0) {
+    nextPos = modalItems.children.length - 1;
+  } else if (currentPos >= modalItems.children.length) {
+    nextPos = 0;
+  }
+
+  const nextItem = modalItems.children[nextPos];
+  if (nextItem) {
+    item.classList.remove('active');
+    nextItem.classList.add('active');
+  }
+}
 
 modalSearch.addEventListener('input', function(e) {
   const fuse = new Fuse(cachedFiles, fuzeOptions);
@@ -51,6 +95,10 @@ modalSearch.addEventListener('input', function(e) {
   for (var i = 0; i < search.length; i++) {
     var div = document.createElement('div');
     div.classList.add('modal-item');
+    if (i === 0) {
+      div.classList.add('active');
+    }
+    div.setAttribute('data-path', search[i].path);
     div.textContent = '...' + search[i].path.slice(-40);
     modalItems.appendChild(div);
   }
@@ -69,7 +117,9 @@ function toggleTabs() {
 function toggleModal() {
   modal.classList.toggle('visible');
   if (modal.classList.contains('visible')) {
-    setTimeout(() => {modalSearch.focus() }, 30);
+    setTimeout(() => {
+      modalSearch.focus()
+    }, 30);
   }
 }
 
