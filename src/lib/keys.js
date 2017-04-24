60249571ece280fa8fcdc5d654ab897f9de591b3
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-04-24 16:10:20 -0300

    ES6 adjustments

--- src/lib/keys.js
+++ src/lib/keys.js
@@ -11,20 +11,20 @@ class Keys {
   // TODO: reset method: unbind all keys
 
   editor() {
-    key('⌘+o', function(event, handler) {
+    key('⌘+o', (event, handler) => {
       // TODO: Multiple files and diretory
       openFiles()
     })
 
-    key('⌘+p', function(event, handler) {
+    key('⌘+p', (event, handler) => {
       toggleModal()
     })
 
-    key('⌘+e', function(event, handler) {
+    key('⌘+e', (event, handler) => {
       toggleTabs()
     })
 
-    key('⌘+,', function(event, handler) {
+    key('⌘+,', (event, handler) => {
       // TODO: Preferences
       console.log('Preferences')
     })
